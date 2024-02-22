53645774c05bf7c56a939f84821f7b3db7cf8418
Nino (Nutsa) Kharazishvili <nutsa1999@gmail.com>
2024-02-22 18:42:36 +0400

    added auto slide

--- homework11/script.js
+++ homework11/script.js
@@ -1,3 +1,5 @@
+//filter
+
 function searchProduct() {
     let searchPhrase = document.getElementById("searchInput").value.toUpperCase();
     let productNames = document.querySelectorAll(".product_name")
@@ -9,6 +11,41 @@ function searchProduct() {
             prod.parentElement.style.display = "none";
         }
     })
+}
+
+//Auto
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("auto");
+    let slidesTwo = document.getElementsByClassName("auto2");
+    let slidesThree = document.getElementsByClassName("auto3");
+    let slidesFour = document.getElementsByClassName("auto4");
+
+    slideIndex++;
+
+    autoChage(slides, i, slideIndex);
+    autoChage(slidesTwo, i, slideIndex);
+    autoChage(slidesThree, i, slideIndex);
+    autoChage(slidesFour, i, slideIndex);
+
+    setTimeout(showSlides, 2000);
+}
+
+function autoChage(slides, i) {
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
 
 
 }
+
+//Manual
