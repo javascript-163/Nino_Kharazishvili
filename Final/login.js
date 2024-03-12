c3d7fe6da31bfa1e341fe6eee0809ec93f4b23ac
Nino (Nutsa) Kharazishvili <nutsa1999@gmail.com>
2024-03-12 16:24:09 +0400

    completed login/register functionality

---
+++ Final/login.js
@@ -0,0 +1,32 @@
+
+let msg = document.querySelector(".login_msg");
+let form = document.getElementById("login_form");
+
+if (form) {
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        let email = document.getElementById('email_login').value;
+        let password = document.getElementById('password_login').value;
+
+        let storedData = JSON.parse(localStorage.getItem('formData'));
+
+        if (storedData && email && password) {
+
+            if (storedData.filter(item => item.email === email && item.password === password).length > 0) {
+                window.location.href = "reservation.html";
+            } else if (storedData.filter(item => item.email === email).length > 0) {
+                msg.innerHTML = "Incorrect password";
+                msg.classList.remove("hidden");
+                msg.style.color = "red";
+                form.reset();
+            } else {
+                msg.innerHTML = "You are not registered, please sign up";
+                msg.classList.remove("hidden");
+                msg.style.color = "red";
+                form.reset();
+            }
+        }
+
+    })
+}
