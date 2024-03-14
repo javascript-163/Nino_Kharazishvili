a468c4c354c24801764dddcae9d62c5d93559587
Nino (Nutsa) Kharazishvili <nutsa1999@gmail.com>
2024-03-14 17:05:10 +0400

    completed implementing api menu

--- Final/menu.js
+++ Final/menu.js
@@ -1,24 +1,53 @@
 const apiKey = 'e487bf99cab14278be1a86a25d85d3e4';
 const baseUrl = 'https://api.spoonacular.com/';
-
-// Example endpoint: Get random recipes
 const endpoint = 'food/menuItems/search';
-const query = 'burger';
 
-// Specify the number of results you want to retrieve
-const number = 45;
+const number = 10;
+let query;
 
-// Construct the URL with parameters
-const url = `${baseUrl}${endpoint}?apiKey=${apiKey}&query=${query}&number=${number}`;
+let coffeeBtn = document.getElementById("coffee_btn");
+let sandwichBtn = document.getElementById("sandwich_btn");
+let empanadaBtn = document.getElementById("empanada_btn");
+let dessertBtn = document.getElementById("dessert_btn");
+let menuTypeTitle = document.getElementById("menu_item_type");
 
-// Make the API request
-fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        // Handle the API response
-        console.log(data);
-    })
-    .catch(error => {
-        // Handle errors
+
+async function fetchMenuItems(query) {
+    const url = `${baseUrl}${endpoint}?apiKey=${apiKey}&query=${query}&number=${number}`;
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        data.menuItems.forEach((item, index) => {
+            let menuItem = document.getElementById(`menu_item${index}`);
+            menuItem.innerHTML = `${item.title}`;
+        });
+    } catch (error) {
         console.error('Error fetching data:', error);
-    });
+    }
+}
+
+
+function loadMenuItems(query) {
+    menuTypeTitle.innerHTML = `${query.toUpperCase()}`;
+    fetchMenuItems(query);
+}
+
+window.onload = function () {
+    loadMenuItems("coffee");
+};
+
+coffeeBtn.addEventListener("click", () => {
+    loadMenuItems("coffee");
+});
+
+sandwichBtn.addEventListener("click", () => {
+    loadMenuItems("sandwich");
+});
+
+empanadaBtn.addEventListener("click", () => {
+    loadMenuItems("empanada");
+});
+
+dessertBtn.addEventListener("click", () => {
+    loadMenuItems("dessert");
+});
